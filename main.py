4706b0a25b8adaa687145d893a4c510f03f4da50
candelabp <candepuerta@gmail.com>
2026-08-19 14:54:36 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Crea un código que imprima en pantalla la expresión “Mi Primer Código En Python.”
+
+print("Mi primer código en Python")
+#Crea un código que imprima en pantalla la siguiente expresión.
+
+print("1\tA\tB\tC\n2\tD\tE\tF\n3\tG\tH\tI")
+#Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:¿Qué estás estudiando?
+#El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+
+respuesta = input(" Qué estas estudiando? :")
+
+print(respuesta)
+# Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:¿En qué país vives?
+#El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+
+pais = input("En qué país vives?")
+print(pais)
+# Declara dos variables, llamadas nombre y edad. Asigna a la variable nombre el valor "David Bowman", y a la edad, el valor 51.
+nombre = "David Bowman"
+edad = 51
+#Crea tres variables: nombre, apellido y nombrecompleto. A nombre, asígnale el valor "Julia", y en apellido, asigna el valor "Roberts". Finalmente, construye la variable nombrecompleto concatenando las variables (recuerda sumar un espacio intermedio).
+nombre= "Julia"
+apellido = "Roberts"
+nombrecompleto = nombre  + " " + apellido
